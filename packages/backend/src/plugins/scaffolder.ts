7214100cd53bb65d8587a125846dd6f1f523d7d3
sertaoseracloud <150350916+sertaoseracloud@users.noreply.github.com>
2024-02-26 15:11:48 -0300

    add azure devops

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,5 +1,13 @@
 import { CatalogClient } from '@backstage/catalog-client';
+import { ScmIntegrations } from '@backstage/integration';
+import { createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
 import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import {
+  cloneAzureRepoAction,
+  pushAzureRepoAction,
+  pullRequestAzureRepoAction,
+} from "@parfuemerie-douglas/scaffolder-backend-module-azure-repositories";
+
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
@@ -9,8 +17,24 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [
+    ...builtInActions, 
+    cloneAzureRepoAction({ integrations }),
+    pushAzureRepoAction({ integrations, config: env.config }),
+    pullRequestAzureRepoAction({ integrations }),
+  ]
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
